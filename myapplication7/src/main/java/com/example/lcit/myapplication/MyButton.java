2bca2571a2ac3a6062baf0f66ecb45555eb6a769
qiuweizhong <1291595004@qq.com>
2017-03-16 16:53:25 +0800

    事件分发机制

---
+++ myapplication7/src/main/java/com/example/lcit/myapplication/MyButton.java
@@ -0,0 +1,65 @@
+package com.example.lcit.myapplication;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.Button;
+
+/**
+ * Created by ${qiuweizhong} on 2017/3/16.
+ */
+public class MyButton extends Button {
+    public MyButton(Context context) {
+        super(context);
+        init();
+    }
+
+    public MyButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public MyButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    public void init() {
+
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+//        Log.d("print", "MyButton  dispatchTouchEvent");//点击一次，抬起一次
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Log.d("print", "ACTION_DOWN  dispatchTouchEvent");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.d("print", "ACTION_MOVE  dispatchTouchEvent");
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.d("print", "ACTION_UP  dispatchTouchEvent");
+                break;
+        }
+        return super.dispatchTouchEvent(event);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+//        Log.d("print", "MyButton  onTouchEvent");
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Log.d("print", "ACTION_DOWN  onTouchEvent");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.d("print", "ACTION_MOVE  onTouchEvent");
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.d("print", "ACTION_UP  onTouchEvent");
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+}
